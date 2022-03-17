81375a1ba98f96f8345d593eb005e6193430bd7b
Carolina Larsson <carolinalarsson97@gmail.com>
2022-03-17 11:05:05 +0100

    added clickable green wall

---
+++ js/overlay.js
@@ -0,0 +1,9 @@
+
+// Constants
+const wallGreen = document.getElementById('wall-green');
+const wallGreenOverlay = document.getElementById('wall-green-overlay');
+
+// Functions
+function showWallGreenOverlay() {
+    wallGreenOverlay.style.display = 'flex';
+}
