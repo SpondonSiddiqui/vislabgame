285163f6c6ec7668e02d701febe80278a800d27d
Carolina Larsson <32388780+puttelarsson@users.noreply.github.com>
2022-04-14 14:25:10 +0200

    testing live share commit

--- js/overlay.js
+++ js/overlay.js
@@ -1,9 +1,34 @@
 
 // Constants
+const wallYellow = document.getElementsByClassName('wall-green');
+const wallBlack = document.getElementsByClassName('wall-black');
+const wallRed = document.getElementsByClassName('wall-red');
+const wallBlue = document.getElementsByClassName('wall-blue');
 const wallGreen = document.getElementsByClassName('wall-green');
+
+const yellowOverlay = document.getElementById('wall-yellow-overlay');
+const blackOverlay = document.getElementById('wall-black-overlay');
+const redOverlay = document.getElementById('wall-red-overlay');
+const blueOverlay = document.getElementById('wall-blue-overlay');
 const greenOverlay = document.getElementById('wall-green-overlay');
 
 // Functions
+function showYellowOverlay() {
+    yellowOverlay.style.display = 'flex';
+}
+
+function showBlackOverlay() {
+    blackOverlay.style.display = 'flex';
+}
+
+function showRedOverlay() {
+    redOverlay.style.display = 'flex';
+}
+
+function showBlueOverlay() {
+    blueOverlay.style.display = 'flex';
+}
+
 function showGreenOverlay() {
-    greenOverlay.style.display = 'flex';
+    greeOverlay.style.display = 'flex';
 }
