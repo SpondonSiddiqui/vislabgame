eff7783233d4eb54f30bfd472d6fcff30322bb89
Spondon Siddiqui <spondonsiddiqui@gmail.com>
2022-04-18 10:36:38 +0200

    Red beams working with overlay and fixed exit button for red overlay
    Just changed the z-index of the exit button to 12

--- js/overlay.js
+++ js/overlay.js
@@ -1,24 +1,36 @@
 
 // Constants
+
+// Walls
 const wallYellow = document.getElementById('wall-yellow');
 const wallBlack = document.getElementById('wall-black');
 const wallRed = document.getElementById('wall-red');
 const wallBlue = document.getElementById('wall-blue');
 const wallGreen = document.getElementById('wall-green');
 
+// Yellow beams
 const yellowBeamA = document.getElementById('yellow-beam-a');
 const yellowBeamAPartial1 = document.getElementById('yellow-beam-a-partial-1');
 const yellowBeamAPartial2 = document.getElementById('yellow-beam-a-partial-2');
 const yellowBeamB = document.getElementById('yellow-beam-b');
 const yellowBeamBPartial = document.getElementById('yellow-beam-b-partial');
 
+// Black beams
 const blackBeamA = document.getElementById('black-beam-a');
 const blackBeamB = document.getElementById('black-beam-b');
 
+// Red beams
+const redBeamA = document.getElementById('red-beam-a');
+const redBeamAPartial = document.getElementById('red-beam-a-partial');
+const redBeamB = document.getElementById('red-beam-b');
+const redBeamBPartial = document.getElementById('red-beam-b-partial');
+
+// Green beams
 const greenBeamA = document.getElementById('green-beam-a');
 const greenBeamAPartial = document.getElementById('green-beam-a-partial');
 const greenBeamB = document.getElementById('green-beam-b');
 
+// Overlays
 const yellowOverlay = document.getElementById('wall-yellow-overlay');
 const blackOverlay = document.getElementById('wall-black-overlay');
 const redOverlay = document.getElementById('wall-red-overlay');
@@ -45,6 +57,11 @@ function showBlackOverlay() {
 
 function showRedOverlay() {
     redOverlay.style.display = 'flex';
+
+    redBeamA.style.zIndex = 11;
+    redBeamAPartial.style.zIndex = 11;
+    redBeamB.style.zIndex = 11;
+    redBeamBPartial.style.zIndex = 11;
 }
 
 function showBlueOverlay() {
@@ -74,6 +91,11 @@ function hideOverlays() {
 
     blackBeamA.style.zIndex = 0;
     blackBeamB.style.zIndex = 0;
+
+    redBeamA.style.zIndex = 0;
+    redBeamAPartial.style.zIndex = 0;
+    redBeamB.style.zIndex = 0;
+    redBeamBPartial.style.zIndex = 0;
 
     greenBeamA.style.zIndex = 0;
     greenBeamAPartial.style.zIndex = 0;
