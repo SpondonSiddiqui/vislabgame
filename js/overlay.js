416e2f05f3d3a406d0a8c3c706efcec76b11ad6f
Spondon Siddiqui <spondonsiddiqui@gmail.com>
2022-04-18 10:23:29 +0200

    Yellow beams now show up and disappear correctly when opening overlay

--- js/overlay.js
+++ js/overlay.js
@@ -6,6 +6,12 @@ const wallRed = document.getElementsByClassName('wall-red');
 const wallBlue = document.getElementsByClassName('wall-blue');
 const wallGreen = document.getElementsByClassName('wall-green');
 
+const yellowBeamA = document.getElementById('yellow-beam-a');
+const yellowBeamAPartial1 = document.getElementById('yellow-beam-a-partial-1');
+const yellowBeamAPartial2 = document.getElementById('yellow-beam-a-partial-2');
+const yellowBeamB = document.getElementById('yellow-beam-b');
+const yellowBeamBPartial = document.getElementById('yellow-beam-b-partial');
+
 const greenBeamA = document.getElementById('green-beam-a');
 const greenBeamAPartial = document.getElementById('green-beam-a-partial');
 const greenBeamB = document.getElementById('green-beam-b');
@@ -19,6 +25,12 @@ const greenOverlay = document.getElementById('wall-green-overlay');
 // Functions
 function showYellowOverlay() {
     yellowOverlay.style.display = 'flex';
+
+    yellowBeamA.style.zIndex = 11;
+    yellowBeamAPartial1.style.zIndex = 11;
+    yellowBeamAPartial2.style.zIndex = 11;
+    yellowBeamB.style.zIndex = 11;
+    yellowBeamBPartial.style.zIndex = 11;
 }
 
 function showBlackOverlay() {
@@ -35,6 +47,7 @@ function showBlueOverlay() {
 
 function showGreenOverlay() {
     greenOverlay.style.display = 'flex';
+
     greenBeamA.style.zIndex = 11;
     greenBeamAPartial.style.zIndex = 11;
     greenBeamB.style.zIndex = 11;
@@ -46,6 +59,12 @@ function hideOverlays() {
     blackOverlay.style.display = 'none';
     blueOverlay.style.display = 'none';
     greenOverlay.style.display = 'none';
+
+    yellowBeamA.style.zIndex = 0;
+    yellowBeamAPartial1.style.zIndex = 0;
+    yellowBeamAPartial2.style.zIndex = 0;
+    yellowBeamB.style.zIndex = 0;
+    yellowBeamBPartial.style.zIndex = 0;
 
     greenBeamA.style.zIndex = 0;
     greenBeamAPartial.style.zIndex = 0;
